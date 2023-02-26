1fa97ac404e5e4eee058bee4f3d5f1b87c135a4a
duova <61954674+duova@users.noreply.github.com>
2023-02-26 14:34:53 +0000

    Initial uproject commit.

---
+++ Source/SlotableFramework/SlotableFramework.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SlotableFramework.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlotableFramework, "SlotableFramework" );
---
+++ Source/SlotableFramework/SlotableFrameworkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlotableFrameworkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLOTABLEFRAMEWORK_API ASlotableFrameworkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
