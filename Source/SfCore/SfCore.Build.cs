8ed65531bbfe04e610c6583af0353567760e41a3
duova <61954674+duova@users.noreply.github.com>
2023-02-27 12:09:07 +0000

    Added SfCore module.

---
+++ Source/SfCore/SfCore.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SfCore : ModuleRules
+{
+	public SfCore(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
