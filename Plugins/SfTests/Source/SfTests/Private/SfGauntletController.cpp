8ce65e211dcca003f2aae99ca9ac7ea35f2efe96
Kai Chang <61954674+duova@users.noreply.github.com>
2023-07-18 21:55:52 +0100

    Partial implementation of testing controller.

---
+++ Plugins/SfTests/Source/SfTests/Private/SfGauntletController.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SfGauntletController.h"
+
+void USfGauntletController::OnPostMapChange(UWorld* World)
+{
+	Super::OnPostMapChange(World);
+	FString NetRole;
+	if (World->GetNetMode() == NM_DedicatedServer)
+	{
+		NetRole = "DedicatedServer";
+	}
+	else if (World->GetNetMode() == NM_Client)
+	{
+		NetRole = "Client";
+	}
+	else if (World->GetNetMode() == NM_Standalone)
+	{
+		NetRole = "Standalone";
+	}
+	else if (World->GetNetMode() == NM_ListenServer)
+	{
+		NetRole = "ListenServer";
+	}
+	UE_LOG(LogGauntlet, Display, TEXT("Gauntlet controller loaded into world with role: %s"), *FString(NetRole));
+}
---
+++ Plugins/SfTests/Source/SfTests/Public/SfGauntletController.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GauntletTestController.h"
+#include "UObject/Object.h"
+#include "SfGauntletController.generated.h"
+
+/**
+ * Main test controller for the slotable framework.
+ */
+UCLASS()
+class SFTESTS_API USfGauntletController : public UGauntletTestController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnPostMapChange(UWorld* World) override;
+};
